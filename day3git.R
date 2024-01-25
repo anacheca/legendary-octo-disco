70973bb7fb1860ce0294b6f051724545f6d6e44b
anacheca <ana.martinez-checa2@nhm.ac.uk>
2024-01-25 12:54:30 +0000

    calculations

--- day3git.R
+++ day3git.R
@@ -1,2 +1,11 @@
 #to create this is new folder version control, git and copy the code in git
 #in envi go to git and click - so push to add and commit button
+
+#add more stuff and upload
+#more stuff
+#every time we made a new version of pdf git saves a new version of it and all pdf get hidden everytime you change them and commit
+#in those cases do .gitignore and write a *.pdf or *.png (delete everthing that finish in .png) and check in env gir if it does appear
+#eg. not add raw data - row-data/*
+#you can revert the changes by going to more and revert
+#rebase to delete stuff on the middle
+sum(3+4)
